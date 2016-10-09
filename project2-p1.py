57ac9ba93806f658d20557da88cef53363d111bb
Josue <josueruiz4747@gmail.com>
2016-10-08 20:27:22 -0700

    working on part 1

---
+++ project2-p1.py
@@ -0,0 +1,26 @@
+# from random import choice
+# from string import ascii_lowercase
+# DEFAULT_KEY = "bpzhgocvjdqswkimlutneryaxf"
+# ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+#
+# inv_map = {}
+# for i in range(25):
+#     inv_map[i] = DEFAULT_KEY[i]
+#
+# print(inv_map)
+#
+# print(''.join(choice(ascii_lowercase) for i in range(25)))
+
+import random
+chars = 'abcdefghijklmnopqrstuvwxyz'
+DEFAULT_KEY = 'bpzhgocvjdqswkimlutneryaxf'
+"""Generate an key for our cipher"""
+
+plaintext = "flow"
+
+shuffled = sorted(chars, key=lambda k: random.random())
+
+key = dict(zip(chars, DEFAULT_KEY))
+
+"""Encrypt the string and return the ciphertext"""
+print(''.join(key[a] for a in plaintext))
