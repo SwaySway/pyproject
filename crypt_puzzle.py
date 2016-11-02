fa2f26bbc7a3dec6e8896539d5b29e25c1eced84
Josue <josueruiz4747@gmail.com>
2016-11-01 22:11:28 -0700

    added sample python code for crypt arithmetic puzzle. Dissect the code so it's only data dependency is to use lists

--- crypt_puzzle.py
+++ crypt_puzzle.py
@@ -1,4 +1,20 @@
-print("Crypt Puzzle Solver\nNote"
-      ": No two letters can be the same!")
-crypt_puzzle = input("Enter the function: ")
+from itertools import permutations
 
+# demo code to dissect
+def solve(puzzle):
+    words = [w for w in puzzle.split() if w.isalpha()]
+    print(words)
+    nonzeros = {w[0] for w in words}
+    others = {a for a in ''.join(words) if a not in nonzeros}
+    chars = [ord(c) for c in nonzeros] + [ord(c) for c in others]
+    assert len(chars) < 10, 'Too many letters'
+    for guess in permutations('0123456789', len(chars)):
+        if '0' not in guess[:len(nonzeros)]:
+            print(guess[:len(nonzeros)])
+            equation = puzzle.translate(dict(zip(chars, guess)))
+            if eval(equation):
+                return puzzle, equation
+
+
+if __name__=='__main__':
+    print('\n'.join(solve("send + more == money")))
